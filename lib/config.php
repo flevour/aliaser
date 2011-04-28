3093cdfbab700f477061625402f2a2422bbfccff
Francesco Levorato <git@flevour.net>
2011-04-28 23:39:01 +0200

    moving yaml under lib/vendor

--- lib/config.php
+++ lib/config.php
@@ -5,7 +5,7 @@ spl_autoload_register('autoload_class');
 function autoload_class($class_name)
 {
   $dirname = dirname(__FILE__);
-  $dirs = array("$dirname", "$dirname/yaml/lib/");
+  $dirs = array("$dirname", "$dirname/vendor/yaml/lib/");
   foreach ($dirs as $dir)
   {
     if (file_exists("$dir/$class_name.php"))
